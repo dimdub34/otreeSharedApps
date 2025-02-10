d7bb5df008a042957c8d3e05fc33950258ea1c70
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2025-02-10 15:13:02 +0100

    slider task test file

--- slider_task/tests.py
+++ slider_task/tests.py
@@ -5,5 +5,9 @@ from . import *
 class PlayerBot(Bot):
     def play_round(self):
         yield Instructions
-        yield Submission(Slider, dict(num_successful_sliders=random.randint(C.REQUIRED_SUCCESS-1, C.REQUIRED_SUCCESS +2)))
-        yield Results
+        yield Submission(
+            Slider,
+            dict(num_successful_sliders=random.randint(C.REQUIRED_SUCCESS-1, C.REQUIRED_SUCCESS +2)),
+            check_html=False
+        )
+        yield Submission(Results, check_html=False)
