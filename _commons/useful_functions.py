b012c0608a6c372d689a4ead05b98d23a64106cf
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2026-04-17 06:54:54 +0200

    commons

---
+++ _commons/useful_functions.py
@@ -0,0 +1,8 @@
+def get_minutes(seconds):
+    """Convertit des secondes en minutes et secondes."""
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+    if remaining_seconds:
+        return f"{int(minutes)} minute{'s' if minutes > 1 else ''} et {int(remaining_seconds)} seconde{'s' if remaining_seconds > 1 else ''}"
+    else:
+        return f"{int(minutes)} minute{'s' if minutes > 1 else ''}"
